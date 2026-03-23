05669fcc4e788664ecd384a6626064b472e43530
Nasir Mehmood <nasirmalik1771@gmail.com>
2026-03-23 13:54:26 +0100

    Update uploadToS3.js

--- uploadToS3.js
+++ uploadToS3.js
@@ -2,12 +2,17 @@ require("dotenv").config();
 
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 
+// const s3 = new S3Client({
+//   region: process.env.AWS_REGION,
+//   credentials: {
+//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   },
+// });
+
+/// With IAM policy
 const s3 = new S3Client({
   region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
 });
 
 async function uploadToS3(file) {
